19d4946ce476deedb1433ba6b2ece69840ff471d
Шляпников Андрей Николаевич <andrey.shlyapnikov@sbermarket.ru>
2025-03-01 20:09:26 +0300

    iter 2

--- client.rb
+++ client.rb
@@ -27,18 +27,24 @@ expected_time = 7
 #   - одновременно можно запускать не более одного
 #
 def a(value)
-  puts "https://localhost:9292/a?value=#{value}"
-  Faraday.get("https://localhost:9292/a?value=#{value}").body
+  Thread.new do
+    puts "https://localhost:9292/a?value=#{value}"
+    Faraday.get("https://localhost:9292/a?value=#{value}").body
+  end  
 end
 
 def b(value)
-  puts "https://localhost:9292/b?value=#{value}"
-  Faraday.get("https://localhost:9292/b?value=#{value}").body
+  Thread.new do
+    puts "https://localhost:9292/b?value=#{value}"
+    Faraday.get("https://localhost:9292/b?value=#{value}").body
+  end
 end
 
 def c(value)
-  puts "https://localhost:9292/c?value=#{value}"
-  Faraday.get("https://localhost:9292/c?value=#{value}").body
+  Thread.new do
+    puts "https://localhost:9292/c?value=#{value}"
+    Faraday.get("https://localhost:9292/c?value=#{value}").body
+  end
 end
 
 func_a = proc { |v| a(v) }
@@ -47,27 +53,18 @@ func_c = proc { |v| c(v) }
 
 @result_task_a = []
 
-def start_worker(values_a,  value_b, worker_a, worker_b, worker_c)
+def start_worker(values, worker, count, result)
   barrier = Async::Barrier.new
-  @results_a = []
-  @result_b = ""
-  threads = []
-  Sync do
-    semaphore = Async::Semaphore.new(4, parent: barrier)
-    values_a.each do |value|
-      threads << semaphore.async do
-        @results_a << worker_a.call(value)       
+  Async do
+    semaphore = Async::Semaphore.new(count, parent: barrier)
+    values.map do |value|
+      semaphore.async do
+        result[value] = worker.call(value).value       
       end
-    end
-    threads << semaphore.async do
-      @result_b = worker_b.call(value_b)
-    end
-    threads.map(&:wait)
+    end.map(&:wait)
     ensure
       barrier.stop
     end
-  result_ab = "#{@results_a.sort.join('-')}-#{@result_b}"
-  worker_c.call(result_ab)
 end
 
 # Референсное решение, приведённое ниже работает правильно, занимает ~19.5 секунд
@@ -77,34 +74,93 @@ def collect_sorted(arr)
   arr.sort.join('-')
 end
 
+val1 = [11, 12, 13]
+val2 = [21, 22, 23]
+val3 = [31, 32, 33]
+
+@all_vall_a = [val1, val2, val3]
+@all_vall_b = [1, 2, 3]
+
+@results_task_a = {}
+@results_task_b = {}
+@results_task_c = {}
+
+@threads = []
+
+@threads << Thread.new {
+  @all_vall_a.each do |v|
+    start_worker(v, func_a, 3, @results_task_a)
+  end
+}
+@threads << Thread.new { start_worker(@all_vall_b, func_b ,2, @results_task_b) }
+
+@threads << Thread.new do
+  index_fetch = 0
+  exists = []
+  process = true
+  while process do
+    keys = @results_task_b.keys
+    unless keys.empty?
+      keys.each do |key|
+        next if exists.include?(key)
+        if key == 1
+          if @results_task_a[11] && @results_task_a[12] && @results_task_a[13]
+            payload = "#{[@results_task_a[11], @results_task_a[12], @results_task_a[13]].sort.join("-")}-#{@results_task_b[key]}"
+            start_worker([payload], func_c ,1, @results_task_c)
+            exists << key
+            index_fetch += 1
+          end
+        elsif key == 2
+          if @results_task_a[21] && @results_task_a[22] && @results_task_a[23]
+            payload = "#{[@results_task_a[21], @results_task_a[22], @results_task_a[23]].sort.join("-")}-#{@results_task_b[key]}"
+            start_worker([payload], func_c ,1, @results_task_c)
+            exists << key
+            index_fetch += 1
+          end
+        elsif key == 3
+          if @results_task_a[31] && @results_task_a[32] && @results_task_a[33]
+            payload = "#{[@results_task_a[31], @results_task_a[32], @results_task_a[33]].sort.join("-")}-#{@results_task_b[key]}"
+            start_worker([payload], func_c ,1, @results_task_c)
+            exists << key
+            index_fetch += 1
+          end
+        end
+      end
+    end
+    if index_fetch >= 3
+      process = false 
+      break  
+    end
+  end
+end
 start = Time.now
+@threads.map(&:join)
 
+result = a(@results_task_c.values.sort.join("-")).value
+# ab1 = start_worker(val1, 1, func_a, func_b)
+# # puts "AB1 = #{ab1}"
 
+# c1 = c(ab1)
+# # puts "C1 = #{c1}"
 
-ab1 = start_worker(val, 1, func_a, func_b)
-puts "AB1 = #{ab1}"
+# # val = [21, 22, 23]
 
-c1 = c(ab1)
-puts "C1 = #{c1}"
+# ab2 = start_worker(val2, 2, func_a, func_b)
+# # puts "AB2 = #{ab2}"
 
-val = [21, 22, 23]
+# c2 = c(ab2)
+# # puts "C2 = #{c2}"
 
-ab2 = start_worker(val, 2, func_a, func_b)
-puts "AB2 = #{ab2}"
+# # val = [31, 32, 33]
 
-c2 = c(ab2)
-puts "C2 = #{c2}"
+# ab3 = start_worker(val3, 3, func_a, func_b)
+# # puts "AB3 = #{ab3}"
 
-val = [31, 32, 33]
+# c3 = c(ab3)
+# # puts "C3 = #{c3}"
 
-ab3 = start_worker(val, 3, func_a, func_b)
-puts "AB3 = #{ab3}"
-
-c3 = c(ab3)
-puts "C3 = #{c3}"
-
-c123 = collect_sorted([c1, c2, c3])
-result = a(c123)
+# c123 = collect_sorted([c1, c2, c3])
+# result = a(c123)
 
 
 time = Time.now - start
@@ -116,6 +172,6 @@ if result != expected_result
   raise ResultIsNotEq, "#{result} != #{expected_result}"
 end
 
-if time > expected_time
-  raise TimeIsOverError, "#{time}"
-end
+# if time > expected_time
+#   raise TimeIsOverError, "#{time}"
+# end
