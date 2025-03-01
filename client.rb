94b184f0c770e1dc5c78fa062f82f41cf04fd2f8
Шляпников Андрей Николаевич <andrey.shlyapnikov@sbermarket.ru>
2025-03-01 14:44:45 +0300

    iter 1

--- client.rb
+++ client.rb
@@ -1,7 +1,19 @@
 require 'openssl'
 require 'faraday'
+require 'async'
+require 'async/barrier'
+require 'async/semaphore'
+
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+class TimeIsOverError < StandardError; end
+
+class ResultIsNotEq < StandardError; end
+
+expected_result = "0bbe9ecf251ef4131dd43e1600742cfb"
+
+expected_time = 7
 
 # Есть три типа эндпоинтов API
 # Тип A:
@@ -29,6 +41,35 @@ def c(value)
   Faraday.get("https://localhost:9292/c?value=#{value}").body
 end
 
+func_a = proc { |v| a(v) }
+func_b = proc { |v| b(v) }
+func_c = proc { |v| c(v) }
+
+@result_task_a = []
+
+def start_worker(values_a,  value_b, worker_a, worker_b, worker_c)
+  barrier = Async::Barrier.new
+  @results_a = []
+  @result_b = ""
+  threads = []
+  Sync do
+    semaphore = Async::Semaphore.new(4, parent: barrier)
+    values_a.each do |value|
+      threads << semaphore.async do
+        @results_a << worker_a.call(value)       
+      end
+    end
+    threads << semaphore.async do
+      @result_b = worker_b.call(value_b)
+    end
+    threads.map(&:wait)
+    ensure
+      barrier.stop
+    end
+  result_ab = "#{@results_a.sort.join('-')}-#{@result_b}"
+  worker_c.call(result_ab)
+end
+
 # Референсное решение, приведённое ниже работает правильно, занимает ~19.5 секунд
 # Надо сделать в пределах 7 секунд
 
@@ -38,34 +79,25 @@ end
 
 start = Time.now
 
-a11 = a(11)
-a12 = a(12)
-a13 = a(13)
-b1 = b(1)
 
-ab1 = "#{collect_sorted([a11, a12, a13])}-#{b1}"
+
+ab1 = start_worker(val, 1, func_a, func_b)
 puts "AB1 = #{ab1}"
 
 c1 = c(ab1)
 puts "C1 = #{c1}"
 
-a21 = a(21)
-a22 = a(22)
-a23 = a(23)
-b2 = b(2)
+val = [21, 22, 23]
 
-ab2 = "#{collect_sorted([a21, a22, a23])}-#{b2}"
+ab2 = start_worker(val, 2, func_a, func_b)
 puts "AB2 = #{ab2}"
 
 c2 = c(ab2)
 puts "C2 = #{c2}"
 
-a31 = a(31)
-a32 = a(32)
-a33 = a(33)
-b3 = b(3)
+val = [31, 32, 33]
 
-ab3 = "#{collect_sorted([a31, a32, a33])}-#{b3}"
+ab3 = start_worker(val, 3, func_a, func_b)
 puts "AB3 = #{ab3}"
 
 c3 = c(ab3)
@@ -74,5 +106,16 @@ puts "C3 = #{c3}"
 c123 = collect_sorted([c1, c2, c3])
 result = a(c123)
 
-puts "FINISHED in #{Time.now - start}s."
+
+time = Time.now - start
+
+puts "FINISHED in #{time}s."
 puts "RESULT = #{result}" # 0bbe9ecf251ef4131dd43e1600742cfb
+
+if result != expected_result
+  raise ResultIsNotEq, "#{result} != #{expected_result}"
+end
+
+if time > expected_time
+  raise TimeIsOverError, "#{time}"
+end
